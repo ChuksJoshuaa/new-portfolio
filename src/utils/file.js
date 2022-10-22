902375c8ddf9fcd9b82fa203ae00195124f813f8
D-coder <chuksjoshuaa@gmail.com>
2022-10-22 16:38:28 +0100

    WIP: Added file

--- src/utils/file.js
+++ src/utils/file.js
@@ -37,4 +37,49 @@ export const dataInfo = [
     linkUrl: "https://ecommerce-sanity-cms.vercel.app/",
     stack: ["Next.js", "Tailwindcss", "Sanity-io"],
   },
+  {
+    id: randy(),
+    name: "Comfy Ecommerce Store",
+    imageUrl:
+      "https://res.cloudinary.com/chuksmbanaso/image/upload/v1645406618/media/Screenshot_32_h7ouwu.png",
+    gitUrl: "https://github.com/ChuksJoshuaa/React-comfy-store",
+    linkUrl: "https://comfiee.netlify.app/",
+    stack: ["React.js", "Styled-components", "Auth0Auth"],
+  },
+  {
+    id: randy(),
+    name: "Crytocurrency Alert System",
+    imageUrl:
+      "https://res.cloudinary.com/chuksmbanaso/image/upload/v1666452401/media/Screenshot_277_zeamtr.png",
+    gitUrl: "https://github.com/ChuksJoshuaa/Coin-App",
+    linkUrl: "https://coin-alert.netlify.app/price",
+    stack: ["React.js", "Tailwindcss", "Redux Toolkit"],
+  },
+  {
+    id: randy(),
+    name: "Fitness Application",
+    imageUrl:
+      "https://res.cloudinary.com/chuksmbanaso/image/upload/v1656174077/media/Screenshot_218_uubkjo.png",
+    gitUrl: "https://github.com/ChuksJoshuaa/Fitness-App",
+    linkUrl: "https://g-fit.netlify.app/",
+    stack: ["React.js", "Rapid Api", "Material UI"],
+  },
+  {
+    id: randy(),
+    name: "You-tube Clone",
+    imageUrl:
+      "https://res.cloudinary.com/chuksmbanaso/image/upload/v1662302556/media/Screenshot_233_xei9a2.png",
+    gitUrl: "https://github.com/ChuksJoshuaa/youtube-clone",
+    linkUrl: "https://you-clone.netlify.app/",
+    stack: ["React.js", "Material UI", "Rapid Api"],
+  },
+  {
+    id: randy(),
+    name: "React Razors Site",
+    imageUrl:
+      "https://res.cloudinary.com/chuksmbanaso/image/upload/v1644267958/media/Screenshot_21_ufuirr.png",
+    gitUrl: "https://github.com/ChuksJoshuaa/React-Razors-site",
+    linkUrl: "https://razoy.netlify.app/",
+    stack: ["React.js", "Context Api", "Styled-components"],
+  },
 ];
