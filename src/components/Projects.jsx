ac31279ccd254ae916ccf7417d2e384e968e5c82
Chuks Joshua <chuksmbanasoj@gmail.com>
2022-10-24 12:08:12 +0100

    Updated Changes

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const Projects = () => {
-  return <div>Projects</div>;
+  const { isDark } = useSelector((state) => state.data);
+
+  return (
+    <div
+      className={`w-full h-full flex flex-col ${
+        isDark ? "bg-main-dark-bg" : ""
+      }`}
+    ></div>
+  );
 };
 
 export default Projects;
